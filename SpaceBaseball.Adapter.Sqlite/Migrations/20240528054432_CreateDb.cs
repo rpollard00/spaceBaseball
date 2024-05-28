f3a7946f22a11d26354b21b162c83cb92953e464
Reese Pollard <rpollard@gmail.com>
2024-05-27 22:56:59 -0700

    Update Db with Ability Scores

---
+++ SpaceBaseball.Adapter.Sqlite/Migrations/20240528054432_CreateDb.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaceBaseball.Adapter.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AbilityScores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Strength = table.Column<int>(type: "INTEGER", nullable: false),
+                    Dexterity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Constitution = table.Column<int>(type: "INTEGER", nullable: false),
+                    Intelligence = table.Column<int>(type: "INTEGER", nullable: false),
+                    Wisdom = table.Column<int>(type: "INTEGER", nullable: false),
+                    Charisma = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbilityScores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    HitChance = table.Column<int>(type: "INTEGER", nullable: false),
+                    Fielding = table.Column<int>(type: "INTEGER", nullable: false),
+                    AbilityScoresId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_AbilityScores_AbilityScoresId",
+                        column: x => x.AbilityScoresId,
+                        principalTable: "AbilityScores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_AbilityScoresId",
+                table: "Players",
+                column: "AbilityScoresId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "AbilityScores");
+        }
+    }
+}
