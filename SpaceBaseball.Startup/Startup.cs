bba0d582c621438af843be87c6d02125b22c6755
Reese Pollard <rpollard@gmail.com>
2024-05-30 00:01:11 -0700

    Inject all new dependencies

--- SpaceBaseball.Startup/Startup.cs
+++ SpaceBaseball.Startup/Startup.cs
@@ -3,17 +3,24 @@ using SpaceBaseball.WebAPI;
 using SpaceBaseball.Adapter.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 using SpaceBaseball.Adapter.Sqlite.Adapters;
-using SpaceBaseball.Core.PlayerGenerator;
+using SpaceBaseball.Adapter.Sqlite.Repositories;
+using SpaceBaseball.Core.Adapters.PlayerGenerator;
+using SpaceBaseball.Core.Adapters.TeamGenerator;
 using SpaceBaseball.Core.Ports;
+using SpaceBaseball.Core.Ports.DataPersistence;
+using SpaceBaseball.Core.Services;
+using SpaceBaseball.Core.Services.DataPersistence;
 
 var api = new WebApi(args, options =>
 {
     options.AddScoped<IBaseballDbContext, BaseballDbContext>();
-    options.AddScoped<IPlayerRetriever, PlayerRetriever>();
-    options.AddScoped<IPlayerCreator, PlayerCreator>();
+    options.AddScoped<IPlayerRepository, PlayerRepository>();
+    options.AddScoped<IPlayerCommandService, PlayerCommandService>();
     options.AddScoped<IPlayerGenerator, PlayerGenerator>();
-    options.AddScoped<IPlayerService, PlayerService>(); 
-    //options.AddSingleton<INameGenerator, NameGenerator>();
+    options.AddScoped<ITeamRepository, TeamRepository>();
+    options.AddScoped<ITeamCommandService, TeamCommandService>();
+    options.AddScoped<ITeamGenerator, TeamGenerator>();
+    options.AddScoped<IPlayerQueryService, PlayerQueryService>(); 
 });
 
 await api.RunAsync();
